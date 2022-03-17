0a6f4a73f8b7f1e3cdaa972fdfde5ef15c1e9964
Ahmed Dhaoui <ahmed.dhaoui@techda.de>
2022-03-17 16:44:25 +0100

    init project

---
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Theme functions
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+function twentytweny_child_enqueue_scripts()
+{
+    //enqueue parent theme style
+    wp_enqueue_style(
+        'twentytwenty-parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    //enqueue child theme style
+    wp_enqueue_style(
+    'twentytwenty-child-style',
+    get_stylesheet_directory_uri().'style.css'
+    );
+}
+
+add_action('wp_enqueue_scripts', 'twentytweny_child_enqueue_scripts');
