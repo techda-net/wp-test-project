fbbb03332fe953f41a2c1374b337a834ad508ed0
Ahmed Dhaoui <ahmed.dhaoui@techda.de>
2022-03-18 10:46:17 +0100

    added render content for single & archive

--- wp-content/plugins/open-weather-map-plugin/templates/archive-cities.php
+++ wp-content/plugins/open-weather-map-plugin/templates/archive-cities.php
@@ -5,7 +5,15 @@
 get_header();
 ?>
     <main id="site-content">
-        Some cool cities here !
+        <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post();
+                echo '<div class="entry-content">';
+                echo '<a href="'.get_permalink().'">'.get_the_title().'</a>';
+                echo '</div>';
+            endwhile;
+        endif;
+        ?>
     </main>
 <?php
 get_footer();
--- wp-content/plugins/open-weather-map-plugin/templates/single-cities.php
+++ wp-content/plugins/open-weather-map-plugin/templates/single-cities.php
@@ -5,7 +5,11 @@
 get_header();
 ?>
 <main id="site-content">
-    Here single City
+    <div class="entry-content">
+        <h1><?php echo $post->post_title; ?></h1>
+        <p>Zip: <?php echo get_field('field_zip');?>, Latitude: <?php echo get_field('field_latitude');?>, Longitude: <?php echo get_field('field_longitude');?></p>
+        <p><?php echo OpenWeatherMapApi::get_data_from_owm(get_field('field_latitude'),get_field('field_longitude'))?></p>
+    </div>
 </main>
 
 <?php get_footer(); ?>
